7788f4f89e97e927dd2fdbc7689d7b3887accc58
Devabubakar <abubakarali3w@gmail.com>
2023-02-11 19:46:58 +0300

    added hashmap implementation soln

--- solutions/Hashmap.js
+++ solutions/Hashmap.js
@@ -1,62 +1,77 @@
 class HashMap { 
     constructor() {
-        this.arr = new Array(1) // initialize the array with a length of 8
+        this.arr = new Array(1001) // initialize the array with a length of 1001
         this.nums = 0; // number of elements in the hashmap
     }
 
+    // function to resize the hashmap when the number of elements exceeds the length of the array
     _resize() {
-    const oldArr = this.arr;
-    const newArr = new Array(oldArr.length * 2);
-    this.arr.forEach(bucket => {
-        if (bucket) {
-            bucket.forEach(([key, value]) => {
-                const index = this._hash(key, newArr.length);
-                if (newArr[index]) {
-                    newArr[index].push([key, value]);
-                } else {
-                    newArr[index] = [[key, value]];
-                }
-            });
-        }
-    });
-    this.arr = newArr;
-}
+        const oldArr = this.arr; // store the old array in a variable
+        const newArr = new Array(oldArr.length * 2); // create a new array with double the length of the old array
 
+        // loop through each bucket of the old array and add its elements to the new array
+        oldArr.forEach(bucket => {
+            if (bucket) {
+                bucket.forEach(([key, value]) => {
+                    const index = this._hash(key, newArr.length); // hash the key to get its index in the new array
+                    if (newArr[index]) {
+                        newArr[index].push([key, value]); // if the index is not empty, add the [key, value] pair to the bucket
+                    } else {
+                        newArr[index] = [[key, value]]; // if the index is empty, create a new bucket and add the [key, value] pair
+                    }
+                });
+            }
+        });
+        this.arr = newArr; // set the new array as the main array
+    }
 
+    // function to hash the key and get its index in the array
     _hash(key, tableSize) {   
-        let hash = 17
+        let hash = 17;
         for(let i = 0; i < key.length; i++) { 
-             hash = 13 * hash + key.charCodeAt(i) % tableSize
+             hash = (13 * hash + key.charCodeAt(i)) % tableSize;
         }
-        return hash 
+        return hash;
     }
+
+    // function to set a key value pair in the hashmap
     set(key, value) {
-        this.nums++
-        this.loadFactor = this.nums / this.arr.length
-        if(this.loadFactor > 0.8) {
-            return this._resize()
+        this.nums++; // increment the number of elements in the hashmap
+        // if the number of elements exceeds the length of the array, resize the hashmap
+        if((this.nums / this.arr.length) > 0.8) {
+            return this._resize();
         }
 
-        const index = this._hash(key, this.arr.length) 
+        const index = this._hash(key, this.arr.length); // hash the key to get its index in the array
+        // if the index is not empty, add the [key, value] pair to the bucket
         if(this.arr[index]){
-            this.arr[index].push([key, value])
+            this.arr[index].push([key, value]);
         }else { 
-            this.arr[index] = [[key, value]]
+            // if the index is empty, create a new bucket and add the [key, value] pair
+            this.arr[index] = [[key, value]];
         }
     }
+
+    // function to get the value of a key from the hashmap
     get(key) {
-        const index = this._hash(key,this.arr.length)
+        const index = this._hash(key,this.arr.length); // hash the key to get its index in the array
+        // if the index is empty, return null
         if( this.arr[index] === undefined) {
-            return null
+            return null;
         }
-        return this.arr[index].find(x => x[0] === key)[1]
+        // return the value of the key by finding it in the bucket
+        return this.arr[index].find(x => x[0] === key)[1];
     }
 }
 
-const map = new HashMap()
+const map = new HashMap();
+
+
 map.set("firstName", "value1")
-map.set("lastName", "value2")
-map.set("age", "value3")
-map.set("address", "value4")
+map.set("care", "value2")
+map.set("race", "value3")
+
 console.log(map.arr.length)
-console.log(map.get("age"))
+console.log(map.get("firstName"))
+console.log(map.get("care")) // returns value2 as expected
+console.log(map.get("race")) // returns value3 as expected
---
+++ tempCodeRunnerFile.javascript
@@ -0,0 +1,60 @@
+class HashMap { 
+    constructor() {
+        this.arr = new Array(1) // initialize the array with a length of 8
+        this.nums = 0; // number of elements in the hashmap
+    }
+
+    _resize() {
+    const oldArr = this.arr;
+    const newArr = new Array(oldArr.length * 2);
+    this.arr.forEach(bucket => {
+        if (bucket) {
+            bucket.forEach(([key, value]) => {
+                const index = this._hash(key, newArr.length);
+                if (newArr[index]) {
+                    newArr[index].push([key, value]);
+                } else {
+                    newArr[index] = [[key, value]];
+                }
+            });
+        }
+    });
+    this.arr = newArr;
+}
+
+
+    _hash(key, tableSize) {   
+        let hash = 17
+        for(let i = 0; i < key.length; i++) { 
+             hash = (13 * hash + key.charCodeAt(i) )% tableSize
+        }
+        return hash 
+    }
+    set(key, value) {
+        this.nums++
+        this.loadFactor = this.nums / this.arr.length
+        if(this.loadFactor > 0.8) {
+            return this._resize()
+        }
+
+        const index = this._hash(key, this.arr.length) 
+        if(this.arr[index]){
+            this.arr[index].push([key, value])
+        }else { 
+            this.arr[index] = [[key, value]]
+        }
+    }
+    get(key) {
+        const index = this._hash(key,this.arr.length)
+        if( this.arr[index] === undefined) {
+            return null
+        }
+        return this.arr[index].find(x => x[0] === key)[1]
+    }
+}
+
+const map = new HashMap()
+map.set("care", "value1")
+
+console.log(map.arr.length)
+console.log(map.get("care"))
